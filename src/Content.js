f06b9488e200627e1ab5434c60f9a48e2a33aa9b
anthony <anthonyd.tastet@gmail.com>
2022-11-13 15:33:33 +0100

    Create the clock XML elements

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
-	const handleNameChange = () => {
-		const names = ["LeBon", "LaBrute", "LeTruand"];
-		const randInt = Math.floor(Math.random() * 3);
-		return names[randInt];
-	};
-
-	const currentHour = new Date();
+	const currentTime = new Date();
 
 	return (
 		<main>
-			<p>hello {handleNameChange()} !</p>
 			<p>
-				il est {currentHour.getHours()}:{currentHour.getMinutes()}:
-				{currentHour.getSeconds()} !
+				il est {currentTime.getHours()}:{currentTime.getMinutes()}:
+				{currentTime.getSeconds()} !
 			</p>
+			<div className="clockFrame">
+				<div className="clockDigit clockDigit1">1</div>
+				<div className="clockDigit clockDigit2">2</div>
+				<div className="clockDigit clockDigit3">3</div>
+				<div className="clockDigit clockDigit4">4</div>
+				<div className="clockDigit clockDigit5">5</div>
+				<div className="clockDigit clockDigit6">6</div>
+				<div className="clockDigit clockDigit7">7</div>
+				<div className="clockDigit clockDigit8">8</div>
+				<div className="clockDigit clockDigit9">9</div>
+				<div className="clockDigit clockDigit10">10</div>
+				<div className="clockDigit clockDigit11">11</div>
+				<div className="clockDigit clockDigit12">12</div>
+				<div className="clockDigit clockDigit13">13</div>
+				<div className="clockDigit clockDigit14">14</div>
+				<div className="clockDigit clockDigit15">15</div>
+				<div className="clockDigit clockDigit16">16</div>
+				<div className="clockDigit clockDigit17">17</div>
+				<div className="clockDigit clockDigit18">18</div>
+				<div className="clockDigit clockDigit19">19</div>
+				<div className="clockDigit clockDigit20">20</div>
+				<div className="clockDigit clockDigit21">21</div>
+				<div className="clockDigit clockDigit22">22</div>
+				<div className="clockDigit clockDigit23">23</div>
+				<div className="clockDigit clockDigit24">24</div>
+			</div>
 		</main>
 	);
 };
