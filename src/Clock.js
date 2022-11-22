c38e3094339016774c6a059e7755067c0f182ea0
anthony <anthonyd.tastet@gmail.com>
2022-11-22 17:48:11 +0100

    Fix performance issues due to misuse of setInterval

--- src/Clock.js
+++ src/Clock.js
@@ -13,27 +13,29 @@ const Clock = () => {
 	let minutesRatio = (secondsRatio + time.myMinutes) / 60;
 	let hoursRatio = (minutesRatio + time.myHours) / 24;
 	let angle = {
-		// secondsAngle: secondsRatio * 360,
-		// minutesAngle: minutesRatio * 360,
+		secondsAngle: secondsRatio * 360,
+		minutesAngle: minutesRatio * 360,
 		hoursAngle: hoursRatio * 360,
 	};
 
 	const [rotation, setRotation] = useState({ angle });
 	const updateAngle = () => {
 		angle = {
-			// secondsAngle: secondsRatio * 360,
-			// minutesAngle: minutesRatio * 360,
+			secondsAngle: secondsRatio * 360,
+			minutesAngle: minutesRatio * 360,
 			hoursAngle: hoursRatio * 360,
 		};
 		setRotation(angle);
 	};
-	// setInterval(updateAngle, 1000);
+	setTimeout(updateAngle, 1000);
 
 	return (
 		<div className="clock__container">
 			<svg className="clock__circle__frame">
 				<circle
 					className="clock__circle"
+					viewBox="0 0 100 100"
+					preserveAspectRatio="xMaxYMid meet"
 					cx="50%"
 					cy="50%"
 					r="40%"
@@ -46,13 +48,11 @@ const Clock = () => {
 					<span className="hours">
 						{time.myHours < 10 ? "0" + time.myHours : time.myHours}
 					</span>
-					<span>:</span>
 					<span className="minutes">
-						{time.myMinutes < 10 ? "0" + time.myMinutes : time.myMinutes}
+						:{time.myMinutes < 10 ? "0" + time.myMinutes : time.myMinutes}
 					</span>
-					<span>:</span>
 					<span className="seconds">
-						{time.mySeconds < 10 ? "0" + time.mySeconds : time.mySeconds}
+						:{time.mySeconds < 10 ? "0" + time.mySeconds : time.mySeconds}
 					</span>
 				</div>
 			</div>
