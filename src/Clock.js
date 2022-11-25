69a6078c310e2337125bdbbfd942b7d64753313f
anthony <anthonyd.tastet@gmail.com>
2022-11-26 00:14:18 +0100

    Add date

--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { format } from 'date-fns';
 
 const Clock = () => {
+	//current date
+	let date = {
+		myDayWord: format(new Date(), 'EEE') + ' .',
+		myDayNumber: format(new Date(), 'dd'),
+		myMonthWord: format(new Date(), 'LLL') + ' .',
+	};
+	console.log(date);
+
 	//current time
 	let time = {
 		myHours: new Date().getHours(),
@@ -30,30 +39,38 @@ const Clock = () => {
 	setTimeout(updateAngle, 1000);
 
 	return (
-		<div className="clock__container">
-			<svg className="clock__circle__frame">
+		<div className='clock__container'>
+			<svg className='clock__circle__frame'>
 				<circle
-					className="clock__circle"
-					viewBox="0 0 100 100"
-					preserveAspectRatio="xMaxYMid meet"
-					cx="50%"
-					cy="50%"
-					r="49.4%"
+					className='clock__circle'
+					viewBox='0 0 100 100'
+					preserveAspectRatio='xMaxYMid meet'
+					cx='50%'
+					cy='50%'
+					r='49.4%'
 					pathLength={360}
-					strokeDasharray={angle.hoursAngle}
+					strokeDasharray={1000}
+					strokeDashoffset={-angle.hoursAngle}
 				></circle>
 			</svg>
-			<div className="clock__time__container">
-				<div className="clock__time">
-					<span className="hours">
-						{time.myHours < 10 ? "0" + time.myHours : time.myHours}
-					</span>
-					<span className="minutes">
-						:{time.myMinutes < 10 ? "0" + time.myMinutes : time.myMinutes}
-					</span>
-					<span className="seconds">
-						:{time.mySeconds < 10 ? "0" + time.mySeconds : time.mySeconds}
-					</span>
+			<div className='clock__text__container'>
+				<div className='clock__time__container'>
+					<div className='clock__time'>
+						<span className='hours'>
+							{time.myHours < 10 ? '0' + time.myHours : time.myHours}
+						</span>
+						<span className='minutes'>
+							:{time.myMinutes < 10 ? '0' + time.myMinutes : time.myMinutes}
+						</span>
+						<span className='seconds'>
+							:{time.mySeconds < 10 ? '0' + time.mySeconds : time.mySeconds}
+						</span>
+					</div>
+				</div>
+				<div className='clock__date__container'>
+					<span className='day--word'>{date.myDayWord}</span>
+					<span className='day--number'>{date.myDayNumber}</span>
+					<span className='month--word'>{date.myMonthWord}</span>
 				</div>
 			</div>
 		</div>
